6457f86e7a340a1c441a4573f1720d01c49a6b40
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-12-24 16:30:08 +0900

    백준 DP 2문제, LCS md file 추가

---
+++ 백준/다이나믹 프로그래밍/1904(01타일).py
@@ -0,0 +1,11 @@
+import sys
+
+sys.stdin = open("input.txt")
+
+size = int(sys.stdin.readline())
+dp = [0] * 1000001
+dp[1] = 1
+dp[2] = 2
+for i in range(3, size + 1):
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
+print(dp[size])
